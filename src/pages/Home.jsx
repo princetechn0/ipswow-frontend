855931464248a3c187c50d8355698746f443c91c
princetechn0 <appleluvver8@gmail.com>
2024-02-09 11:53:11 -0800

    feature(client): favicon and title

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import TableList from "../components/TableList";
 import DownloadModal from "../components/DownloadModal";
 
