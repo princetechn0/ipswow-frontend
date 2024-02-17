43cb13678642a9715bf683d2b8f6e13c67ea6084
princetechn0 <appleluvver8@gmail.com>
2024-02-17 14:20:25 -0800

    feature(client): collape card when preset list too long

--- src/components/PresetCard.jsx
+++ src/components/PresetCard.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import "../styling/Hover.css";
 
 function PresetCard({ data, handlePresetClick }) {
@@ -8,18 +7,34 @@ function PresetCard({ data, handlePresetClick }) {
       className="preset-card hvr-grow"
     >
       <h3 className="preset-card-title">{data.name}</h3>
-      {data.value.map((element, index) => (
-        <DeviceNameItem key={index} name={element.name} />
-      ))}
+      <DeviceNameList devices={data.value} />
     </div>
   );
+}
+
+function DeviceNameList({ devices }) {
+  const AMOUNT_TO_COLLAPSE = 4;
+  const collapsed = devices.length > AMOUNT_TO_COLLAPSE;
+  const trimmedDeviceList = [];
+  if (collapsed) {
+    for (let i = 0; i < AMOUNT_TO_COLLAPSE; i++) {
+      trimmedDeviceList.push(<DeviceNameItem key={i} name={devices[i].name} />);
+    }
+    trimmedDeviceList.push(`+ ${devices.length - AMOUNT_TO_COLLAPSE} more`);
+  } else {
+    devices.forEach((device, index) =>
+      trimmedDeviceList.push(<DeviceNameItem key={index} name={device.name} />)
+    );
+  }
+
+  return trimmedDeviceList;
 }
 
 function DeviceNameItem({ name }) {
   const cleanedName = name.trim().split("/");
   const collapsed = cleanedName.length > 2;
   const trimmedString = [];
-  for (let i = 0; i < 3; i++) {
+  for (let i = 0; i < 2; i++) {
     trimmedString.push(<h5 key={i}> {cleanedName[i]} </h5>);
   }
   if (collapsed) {
